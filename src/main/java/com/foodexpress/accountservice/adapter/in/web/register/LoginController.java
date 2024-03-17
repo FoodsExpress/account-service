01d7933fbaf31fd32e91c15ed47c06dc11389bb3
이승구 <leesg107@naver.com>
2024-03-17 21:59:22 +0900

    feat: login 정보 apiresult 로 감싸기

--- src/main/java/com/foodexpress/accountservice/adapter/in/web/register/LoginController.java
+++ src/main/java/com/foodexpress/accountservice/adapter/in/web/register/LoginController.java
@@ -2,12 +2,15 @@ package com.foodexpress.accountservice.adapter.in.web.register;
 
 import com.foodexpress.accountservice.application.port.in.LoginCommand;
 import com.foodexpress.accountservice.application.port.in.LoginUseCase;
+import com.foodexpress.accountservice.common.util.ApiUtil;
 import com.foodexpress.accountservice.domain.JwtToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.foodexpress.accountservice.common.util.ApiUtil.success;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +20,8 @@ public class LoginController {
     private final LoginUseCase loginUseCase;
 
     @PostMapping
-    public JwtToken login(@RequestBody LoginRequest loginRequest) {
-        return loginUseCase.login(LoginCommand.of(loginRequest.getEmail(), loginRequest.getPassword()));
+    public ApiUtil.ApiResult<JwtToken> login(@RequestBody LoginRequest loginRequest) {
+        return success(loginUseCase.login(LoginCommand.of(loginRequest.getEmail(), loginRequest.getPassword())));
     }
 
 }
