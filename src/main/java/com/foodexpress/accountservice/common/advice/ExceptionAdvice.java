5861bfb61281d9207ad5909f864cc2de7dfae790
이승구 <leesg107@naver.com>
2024-04-17 15:49:55 +0900

    feat: 예외 핸들러 토큰 예외 추가

--- src/main/java/com/foodexpress/accountservice/common/advice/ExceptionAdvice.java
+++ src/main/java/com/foodexpress/accountservice/common/advice/ExceptionAdvice.java
@@ -3,14 +3,23 @@ package com.foodexpress.accountservice.common.advice;
 import com.foodexpress.accountservice.common.advice.exceptions.AlreadyPresentAccountException;
 import com.foodexpress.accountservice.common.advice.exceptions.NotMatchedPasswordException;
 import com.foodexpress.accountservice.common.advice.exceptions.NotValidAccountException;
+import com.foodexpress.accountservice.common.advice.exceptions.NotValidJwtTokenException;
+import com.foodexpress.accountservice.common.util.ApiUtil;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.foodexpress.accountservice.common.util.ApiUtil.fail;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)
+    protected ApiUtil.ApiResult<Void> defaultException(Exception e) {
+        e.printStackTrace();
+        return fail(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler({
         NotValidAccountException.class,
@@ -18,8 +27,18 @@ public class ExceptionAdvice {
         AlreadyPresentAccountException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<?> notValidAccountException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(e.getMessage());
+    protected ApiUtil.ApiResult<Void> notValidAccountException(Exception e) {
+        e.printStackTrace();
+        return fail(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({
+        NotValidJwtTokenException.class
+    })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    protected ApiUtil.ApiResult<Void> unauthorizedException(Exception e) {
+        e.printStackTrace();
+        return fail(e, HttpStatus.UNAUTHORIZED);
     }
 
 }
